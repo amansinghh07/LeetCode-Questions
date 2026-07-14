85d5b787b71931ab1d86d1edd01adcbe07df42eb
Aman kumar <140549982+amansinghh07@users.noreply.github.com>
2026-07-15 03:21:23 +0530

    Time: 22 ms (85.72%), Space: 52.5 MB (6.31%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -1,29 +1,27 @@
 class KthLargest {
- private int K;
-    private PriorityQueue<Integer> pq;
+    int K;
+    PriorityQueue<Integer>pq;
     public KthLargest(int k, int[] nums) {
-     K=k;
-     pq=new PriorityQueue<>();
-     for(int i=0;i<nums.length;i++){
-        if(pq.size()<k)
-        pq.offer(nums[i]);
-        else if(nums[i]>pq.peek()){
-            pq.poll();
-            pq.offer(nums[i]);
+        K=k;
+        pq=new PriorityQueue<>();
+        for(int i:nums){
+            if(pq.size()<K){
+                pq.offer(i);
+            }else if(i>=pq.peek()){
+                pq.poll();
+                pq.offer(i);
+            }
         }
-     }
     }
-
+    
     public int add(int val) {
-      if(pq.size()<K){
-        pq.offer(val);
+        if(pq.size()<K){
+            pq.offer(val);
+        }else if(val>=pq.peek()){
+            pq.poll();
+            pq.offer(val);
+        }
         return pq.peek();
-      }
-      if(val>pq.peek()){
-        pq.poll();
-        pq.offer(val);
-      }
-      return pq.peek();
     }
 }
 
